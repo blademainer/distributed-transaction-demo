313d45e253087738dd38ac0f94e363114d8a0e66
blademainer <blademainer@gmail.com>
2022-03-09 23:43:19 +0800

    complete create and pay method

--- transaction/pay/mock.go
+++ transaction/pay/mock.go
@@ -1,12 +1,46 @@
 package pay
 
+import (
+	"github.com/blademainer/distributed-transaction-demo/mock/rpc"
+)
+
 type mockedOrders struct {
+	rpc rpc.RPC
 }
 
-func (m *mockedOrders) Create(req *Order) (rsp *Order, err error) {
+// NewOrdersServer create orders service
+func NewOrdersServer(rpc rpc.RPC) Orders {
+	orders := &mockedOrders{
+		rpc: rpc,
+	}
+	return orders
+}
 
+// Create new order
+func (m *mockedOrders) Create(req *Order) (rsp *Order, err error) {
+	_, err = m.rpc.Invoke(req)
+	if err != nil {
+		return nil, err
+	}
+	rsp = &Order{
+		ID:       req.ID,
+		Amount:   req.Amount,
+		Currency: req.Currency,
+		Status:   Created,
+	}
+	return
 }
 
 func (m *mockedOrders) Pay(req *Order) (rsp *Order, err error) {
-
+	_, err = m.rpc.Invoke(req)
+	if err != nil {
+		return nil, err
+	}
+	rsp = &Order{
+		ID:       req.ID,
+		Amount:   req.Amount,
+		Currency: req.Currency,
+		Status:   Succeed,
+	}
+	return
 }
